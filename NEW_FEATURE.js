9e421abd50f560fc71422c31f3d3447b154a7e13
Pak MinSeok <pakms980319@gmail.com>
2025-04-11 14:52:41 +0900

    add new feature

---
+++ NEW_FEATURE.js
@@ -0,0 +1,13 @@
+// THIS IS A NEW FEATURE (NOT BREAKING CHANGE!!)
+
+SharedArrayBufferDS
+FileSystemFileEntryDSF
+AbortControllerFD
+SVGFEBlendElementDS
+fdescribeAF
+diffStringsRawDSA
+failFD
+SVGAElementF
+SafeArraySD
+FileASDF
+DataTransferF
